21deaceddaf75bde9a59cd9c13e07b13f806e676
Nowrose Muhammad (Ragib) <nowrose.ragib@vivasoftltd.com>
2026-03-07 14:53:40 +0600

    DFP-3 Refactor: Add `SessionLogger` unit tests
    - Updated `SessionLogger` interface with improved documentation for `setEnabled` and `setLevelStatus`.
    - Refactored `SessionLoggerImpl` to use explicit initialization for `enabled` and `allowedLevels` fields.
    - Added `@visibleForTesting` constructors and getters to `SessionLoggerImpl` to facilitate better testing.
    - Added comprehensive unit tests for `SessionLoggerImpl` covering logger enablement, log level filtering, and default state.
    
    Files added:
    - analysis_server_core/test/lib/lib/src/services/logger/session_logger_test.dart

--- analysis_server_core/lib/src/services/logger/session_logger.dart
+++ analysis_server_core/lib/src/services/logger/session_logger.dart
@@ -4,13 +4,20 @@ import 'package:meta/meta.dart';
 enum SessionLogLevel { INFO, WARNING, ERROR }
 
 abstract interface class SessionLogger {
-  /// Whether the logger is enabled.
-  bool get enabled;
-
   /// Enable or disable the entire logger.
+  ///
+  /// **Note:**
+  /// - This does not alter the enabled/disabled state of log levels.
+  /// - Disabling the logger will completely disable logging.
+  ///   This is the main switch. To enable/disable individual
+  ///   log levels, use [setLevelStatus].
   void setEnabled({required bool enabled});
 
   /// Enable or disable the specified log level.
+  ///
+  /// **Note:**
+  /// - This is a level-specific switch. To enable/disable the entire logger,
+  ///   use [setEnabled].
   void setLevelStatus(SessionLogLevel level, {required bool enabled});
 
   /// Log an info message.
@@ -44,20 +51,34 @@ abstract interface class SessionLogger {
 class SessionLoggerImpl implements SessionLogger {
   final CompositeLogger _compositeLogger;
 
-  var _enabled = true;
-  final _allowedLevels = SessionLogLevel.values.toSet();
+  bool _enabled;
+  Set<SessionLogLevel> _allowedLevels;
 
   SessionLoggerImpl(Map<String, Logger> loggers)
-    : this._(CompositeLogger(loggers.map(MapEntry.new), []));
+    : this._(CompositeLogger(loggers.map(MapEntry.new), []), true, {
+        SessionLogLevel.INFO,
+        SessionLogLevel.WARNING,
+        SessionLogLevel.ERROR,
+      });
 
   @visibleForTesting
-  SessionLoggerImpl.test(CompositeLogger compositeLogger)
-    : this._(compositeLogger);
+  SessionLoggerImpl.test(
+    CompositeLogger compositeLogger, {
+    required bool enabled,
+    required Set<SessionLogLevel> allowedLevels,
+  }) : this._(compositeLogger, enabled, allowedLevels);
 
-  SessionLoggerImpl._(this._compositeLogger);
+  SessionLoggerImpl._(
+    this._compositeLogger,
+    this._enabled,
+    this._allowedLevels,
+  );
 
-  @override
+  @visibleForTesting
   bool get enabled => _enabled;
+
+  @visibleForTesting
+  Set<SessionLogLevel> get allowedLevels => Set.unmodifiable(_allowedLevels);
 
   @override
   void setEnabled({required bool enabled}) {
