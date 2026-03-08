8baa1ed7e898ba83aaab4c9eb504291535fe28e0
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-03-08 23:12:14 +0600

    DFP-6 Style: Wrap tests in a `group` in `runnable_test.dart`

--- functions/test/lib/src/runnable_test.dart
+++ functions/test/lib/src/runnable_test.dart
@@ -2,15 +2,17 @@ import 'package:functions/src/runnable.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Returns result when runnable succeeds', () {
-    final result = runCatching(() => 42, defaultValue: 0);
-    expect(result, 42);
-  });
+  group('runCatching', () {
+    test('Returns result when runnable succeeds', () {
+      final result = runCatching(() => 42, defaultValue: 0);
+      expect(result, 42);
+    });
 
-  test('Returns defaultValue when runnable throws', () {
-    const defaultValue = 99;
-    final result =
-        runCatching(() => throw Exception('oops'), defaultValue: defaultValue);
-    expect(result, defaultValue);
+    test('Returns defaultValue when runnable throws', () {
+      const defaultValue = 99;
+      final result = runCatching(() => throw Exception('oops'),
+          defaultValue: defaultValue);
+      expect(result, defaultValue);
+    });
   });
 }
