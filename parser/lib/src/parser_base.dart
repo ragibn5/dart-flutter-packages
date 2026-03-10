303992afd5f33456d0baf3b09e4fdadbf7d9161e
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-03-10 12:28:29 +0600

    DFP-17 Refactor: Extract parser registry logic into a standalone service
    - Introduced `ParserRegistry<I>` in a new service file to manage the registration and retrieval of decoders.
    - Updated `Parser<I>` to extend `ParserRegistry<I>`, delegating the decoder mapping logic to the new base class.
    - Added a `@visibleForTesting` constructor to `ParserRegistry` to allow for dependency injection during testing.
    
    Files added:
    - `parser/lib/src/services/parser_registry.dart`
    
    Files modified:
    - `parser/lib/src/parser_base.dart`

--- parser/lib/src/parser_base.dart
+++ parser/lib/src/parser_base.dart
@@ -1,34 +1,6 @@
-import 'package:parser/src/types/types.dart';
+import 'package:parser/src/services/parser_registry.dart';
 
-abstract class Parser<I> {
-  final Map<Type, Coder<I, dynamic>> _parserMap = {};
-
-  /// **Get the decoder for the requested type.**
-  /// <br>
-  /// You must specify the type parameter,
-  /// otherwise it may return null, or generic type.
-  ///
-  /// If a decoded was added earlier (with [addDecoder] method call),
-  /// it is guaranteed to return the decoder for that type, otherwise
-  /// it returns null.
-  Coder<I, ResultType>? getDecoder<ResultType>() {
-    final parser = _parserMap[ResultType];
-    if (parser != null) {
-      return parser as Coder<I, ResultType>;
-    } else {
-      return null;
-    }
-  }
-
-  /// **Adds a the supplied decoder to the supported decoder list.**
-  /// <br>
-  /// Later, the decoder will internally use this to decode
-  /// the encoded data to the expected type. If the decoder
-  /// was not added, you should throw an exception or take a similar action.
-  void addDecoder<ResultType>(Coder<I, ResultType> decoder) {
-    _parserMap[ResultType] = decoder;
-  }
-
+abstract class Parser<I> extends ParserRegistry<I> {
   /// **Encode [data].**
   /// <br>
   /// If it is a primitive construct, should return as it is.
