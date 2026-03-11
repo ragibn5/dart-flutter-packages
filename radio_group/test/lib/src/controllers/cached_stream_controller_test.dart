836fa622e7ac0a230711d28de7246c4d3d4c4663
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-03-11 10:43:44 +0600

    DFP-20 Add unit tests for `CachedStreamController`
    - Implemented unit tests for `CachedStreamController` to verify that `add` correctly updates the `lastItem` cache and delegates to the underlying `StreamController`.
    - Added a test case to ensure the `close` method properly terminates the internal controller.
    - Utilized `mocktail` to mock `Stream` and `StreamController` for isolated testing.
    
    Files added:
    - `radio_group/test/lib/src/controllers/cached_stream_controller_test.dart`

---
+++ radio_group/test/lib/src/controllers/cached_stream_controller_test.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:radio_group/src/controllers/cached_stream_controller.dart';
+
+class _MockStream extends Mock implements Stream<int> {}
+
+class _MockStreamController extends Mock implements StreamController<int> {}
+
+void main() {
+  late _MockStream mockStream;
+  late _MockStreamController mockController;
+
+  late CachedStreamController<int> sut;
+
+  setUp(() {
+    mockStream = _MockStream();
+    mockController = _MockStreamController();
+
+    sut = CachedStreamController.test(mockController);
+
+    when(() => mockController.add(any())).thenAnswer((_) {});
+    when(() => mockController.close()).thenAnswer((_) async {});
+    when(() => mockController.stream).thenAnswer((_) => mockStream);
+  });
+
+  test('add(...) should add item to stream and store the last item', () {
+    const itemToAdd = 1;
+
+    sut.add(itemToAdd);
+
+    expect(sut.lastItem, itemToAdd);
+    verify(() => mockController.add(itemToAdd)).called(1);
+  });
+
+  test('close should close the underlying stream', () {
+    sut.close();
+
+    verify(() => mockController.close()).called(1);
+  });
+}
