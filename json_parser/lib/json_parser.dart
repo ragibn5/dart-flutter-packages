66e36940d95928d3c343c179152b19fa37816126
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-03-22 21:56:00 +0600

    DFP-24 WIP-Refactor: Add `json_parser_annotations` dependency to `json_parser`
    - Added `json_parser_annotations` as a path dependency in `pubspec.yaml`.
    - Exported `package:json_parser_annotations/json_parser_annotations.dart` from the main `json_parser.dart` library to make annotations available to consumers.
    
    Files modified:
    - `json_parser/lib/json_parser.dart`
    - `json_parser/pubspec.yaml`

--- json_parser/lib/json_parser.dart
+++ json_parser/lib/json_parser.dart
@@ -1,6 +1,7 @@
 /// The `json_parser` package.
 library;
 
+export 'package:json_parser_annotations/json_parser_annotations.dart';
 export 'package:parser/parser.dart';
 
 export 'src/parsers/bool_parser.dart';
