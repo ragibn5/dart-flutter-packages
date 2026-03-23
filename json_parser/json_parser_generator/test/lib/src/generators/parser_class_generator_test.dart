e9c89d71904e16d709d2d4cf0a351f19d71674d5
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-03-23 21:51:24 +0600

    DFP-24 Test: Add unit tests for `ParserClassGenerator`
    - Implemented `ParserClassGenerator` tests using `mocktail` and `code_builder`.
    - Added test coverage for generating the parser class structure, including correct naming and interface implementation.
    - Verified the generation of `encode` and `decode` methods with proper parameter names and JSON method mapping.
    
    Files added:
    - `json_parser/json_parser_generator/test/lib/src/generators/parser_class_generator_test.dart`

---
+++ json_parser/json_parser_generator/test/lib/src/generators/parser_class_generator_test.dart
@@ -0,0 +1,70 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'package:code_builder/code_builder.dart';
+import 'package:json_parser_generator/src/generators/parser_class_generator.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class _MockClassElement extends Mock implements ClassElement {}
+
+class _MockLibraryElement extends Mock implements LibraryElement {}
+
+void main() {
+  const config = ParserClassGeneratorConfig(
+    classSuffix: 'JsonParser',
+    jsonParserUri: 'package:json_parser/json_parser.dart',
+    encodeParamName: 'value',
+    decodeParamName: 'encoded',
+  );
+
+  late DartEmitter emitter;
+
+  late _MockClassElement mockClassElement;
+  late _MockLibraryElement mockLibraryElement;
+
+  late ParserClassGenerator generator;
+
+  setUp(() {
+    emitter = DartEmitter(useNullSafetySyntax: true);
+
+    mockClassElement = _MockClassElement();
+    mockLibraryElement = _MockLibraryElement();
+
+    generator = const ParserClassGenerator.test(config);
+
+    when(() => mockClassElement.displayName).thenReturn('User');
+    when(() => mockClassElement.library).thenReturn(mockLibraryElement);
+    when(
+      () => mockLibraryElement.uri,
+    ).thenReturn(Uri.parse('package:example/user.dart'));
+  });
+
+  String generate() =>
+      generator.generate(mockClassElement).accept(emitter).toString();
+
+  test('Generates class with correct name and super class specifications', () {
+    expect(
+      generate(),
+      matches(
+        'class\\s*User${config.classSuffix}\\s*implements\\s*Parser<User,\\s*Map<String,\\s*dynamic>>\\s*\\{[\\s\\S]*\\}',
+      ),
+    );
+  });
+
+  test('Generated encode method is correct', () {
+    expect(
+      generate(),
+      matches(
+        '@override\\s*Map<String,\\s*dynamic>\\s*encode\\s*\\(\\s*User\\s*${config.encodeParamName}\\s*\\)\\s*=>\\s*${config.encodeParamName}\\.toJson\\(\\)',
+      ),
+    );
+  });
+
+  test('Generated decode method is correct', () {
+    expect(
+      generate(),
+      matches(
+        '@override\\s*User\\s*decode\\s*\\(\\s*Map<String,\\s*dynamic>\\s*${config.decodeParamName}\\s*\\)\\s*=>\\s*User\\.fromJson\\(${config.decodeParamName}\\)',
+      ),
+    );
+  });
+}
