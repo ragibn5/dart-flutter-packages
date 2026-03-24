2126f00fbcc2d8720d35ee16e319a3e1324c58a9
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-03-24 19:07:01 +0600

    DFP-24 Refactor: Remove unused `SessionDataFactoryConfig` from `generator_core`
    - Deleted the `SessionDataFactoryConfig` model class and its associated mapping logic.
    - Updated the `generator_core` library exports to remove the reference to the deleted model.
    
    Files modified:
    - `generator_core/lib/generator_core.dart`
    
    Files deleted:
    - `generator_core/lib/src/models/session_data_factory_config.dart`

--- generator_core/lib/generator_core.dart
+++ generator_core/lib/generator_core.dart
@@ -6,7 +6,6 @@ export 'package:build/build.dart';
 export 'src/models/context_config.dart';
 export 'src/models/log_config.dart';
 export 'src/models/package_info.dart';
-export 'src/models/session_data_factory_config.dart';
 export 'src/services/config/context_config_loader.dart';
 export 'src/services/logger/session_logger.dart';
 export 'src/services/session/session_data_manager.dart';
