185272be4a61a4e2a66212222f6a907900ee0d69
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-03-24 21:49:42 +0600

    DFP-24 Refactor: Add unit tests for `SessionManagedRawBuilder`
    - Introduced comprehensive unit tests for `SessionManagedRawBuilder` using `mocktail` for dependency mocking.
    - Verified that session start information is logged only when a session is newly created.
    - Validated that `buildWithSession` is invoked with the correct `BuildSessionContext` when the configuration type matches.
    - Ensured a warning is logged and `buildWithSession` is not called if the session configuration type is mismatched.
    
    Files added:
    - `generator_core/test/lib/src/builders/session_managed_raw_builder_test.dart`

---
+++ generator_core/test/lib/src/builders/session_managed_raw_builder_test.dart
@@ -0,0 +1,164 @@
+// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: avoid_redundant_argument_values
+
+import 'package:generator_core/generator_core.dart';
+import 'package:generator_core/src/models/build_session_context.dart';
+import 'package:generator_core/src/models/session_data.dart';
+import 'package:generator_core/src/models/session_data_fetch_result.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+class _MockSessionDataManager extends Mock implements SessionDataManager {}
+
+class _MockBuildStep extends Mock implements BuildStep {}
+
+class _MockSessionData extends Mock implements SessionData {}
+
+class _MockSessionDataFetchResult extends Mock
+    implements SessionDataFetchResult {}
+
+class _MockSessionLogger extends Mock implements SessionLogger {}
+
+class _MockContextConfig extends Mock implements ContextConfig {}
+
+class _TestContextConfig extends ContextConfig {
+  const _TestContextConfig({
+    required super.packageInfo,
+    required super.logConfig,
+  });
+
+  @override
+  Map<String, dynamic> toMap() => {};
+}
+
+class _TestSessionManagedRawBuilder
+    extends SessionManagedRawBuilder<_TestContextConfig> {
+  BuildSessionContext<_TestContextConfig>? capturedSessionContext;
+
+  _TestSessionManagedRawBuilder(super.builderOptions, super.sessionDataManager);
+
+  @override
+  Map<String, List<String>> get buildExtensions => {};
+
+  @override
+  Future<void> buildWithSession(
+    BuildStep buildStep,
+    BuildSessionContext<_TestContextConfig> sessionContext,
+  ) async {
+    capturedSessionContext = sessionContext;
+  }
+}
+
+void main() {
+  const buildOptions = BuilderOptions({'key': 'value'});
+  const testConfig = _TestContextConfig(
+    packageInfo: PackageInfo(name: 'name', location: 'location'),
+    logConfig: LogConfig(
+      logDirectoryRelativePathFromProjectRoot: 'logs',
+      enabled: true,
+      allowInfoLog: true,
+      allowWarningLog: true,
+      allowErrorLog: true,
+    ),
+  );
+
+  late _MockSessionDataManager mockSessionDataManager;
+  late _MockBuildStep mockBuildStep;
+  late _MockSessionData mockSessionData;
+  late _MockSessionDataFetchResult mockFetchResult;
+  late _MockSessionLogger mockSessionLogger;
+  late _TestSessionManagedRawBuilder sut;
+
+  setUp(() {
+    mockSessionDataManager = _MockSessionDataManager();
+    mockBuildStep = _MockBuildStep();
+    mockSessionData = _MockSessionData();
+    mockFetchResult = _MockSessionDataFetchResult();
+    mockSessionLogger = _MockSessionLogger();
+
+    sut = _TestSessionManagedRawBuilder(buildOptions, mockSessionDataManager);
+
+    when(
+      () =>
+          mockSessionDataManager.getSessionDataFor(mockBuildStep, buildOptions),
+    ).thenAnswer((_) async => mockFetchResult);
+    when(() => mockFetchResult.sessionData).thenReturn(mockSessionData);
+    when(() => mockSessionData.logger).thenReturn(mockSessionLogger);
+    when(() => mockSessionData.config).thenReturn(testConfig);
+    when(() => mockFetchResult.isNewlyCreated).thenReturn(false);
+  });
+
+  test('Should log session start info when session is newly created', () async {
+    when(() => mockFetchResult.isNewlyCreated).thenReturn(true);
+    when(
+      () => mockSessionLogger.logInfo(
+        tag: any(named: 'tag'),
+        message: any(named: 'message'),
+        extras: any(named: 'extras'),
+      ),
+    ).thenReturn(null);
+
+    await sut.build(mockBuildStep);
+
+    verify(
+      () => mockSessionLogger.logInfo(
+        tag: any(named: 'tag'),
+        message: any(named: 'message'),
+        extras: any(named: 'extras'),
+      ),
+    ).called(1);
+  });
+
+  test(
+    'Should not log session start info when session is not newly created',
+    () async {
+      when(() => mockFetchResult.isNewlyCreated).thenReturn(false);
+
+      await sut.build(mockBuildStep);
+
+      verifyNever(
+        () => mockSessionLogger.logInfo(
+          tag: any(named: 'tag'),
+          message: any(named: 'message'),
+          extras: any(named: 'extras'),
+        ),
+      );
+    },
+  );
+
+  test(
+    'Should call buildWithSession with correct session context when config type matches',
+    () async {
+      await sut.build(mockBuildStep);
+
+      expect(sut.capturedSessionContext, isNotNull);
+      expect(sut.capturedSessionContext!.config, testConfig);
+      expect(sut.capturedSessionContext!.logger, mockSessionLogger);
+    },
+  );
+
+  test(
+    'Should log warning and not call buildWithSession when config type does not match',
+    () async {
+      final mockContextConfig = _MockContextConfig();
+      when(() => mockSessionData.config).thenReturn(mockContextConfig);
+      when(
+        () => mockSessionLogger.logWarning(
+          tag: any(named: 'tag'),
+          message: any(named: 'message'),
+        ),
+      ).thenReturn(null);
+
+      await sut.build(mockBuildStep);
+
+      verify(
+        () => mockSessionLogger.logWarning(
+          tag: any(named: 'tag'),
+          message: any(named: 'message'),
+        ),
+      ).called(1);
+      expect(sut.capturedSessionContext, isNull);
+    },
+  );
+}
