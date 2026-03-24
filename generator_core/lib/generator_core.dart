1535a55ea3ca98b8e7b41e1a43d4ae441a366cb7
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-03-24 10:55:38 +0600

    DFP-24 Refactor: Remove the `generator_core` package
    - Deleted the `generator_core` package including all source code, models, and services.
    - Removed core abstract classes: `BuilderMetadata`, `ClassMetadata`, `ConfigReader`, and `PostBuildGenerator`.
    - Removed package configuration files: `pubspec.yaml`, `analysis_options.yaml`, `CHANGELOG.md`, and `README.md`.
    - Deleted project metadata, example files, and unit tests associated with the package.
    
    Note:
    A replacement package will be created with same name.
    
    Files deleted:
    - `generator_core/.fvmrc`
    - `generator_core/.gitignore`
    - `generator_core/CHANGELOG.md`
    - `generator_core/README.md`
    - `generator_core/analysis_options.yaml`
    - `generator_core/example/example.dart`
    - `generator_core/generator_core.iml`
    - `generator_core/lib/generator_core.dart`
    - `generator_core/lib/src/models/builder_metadata.dart`
    - `generator_core/lib/src/models/class_metadata.dart`
    - `generator_core/lib/src/services/config_reader.dart`
    - `generator_core/lib/src/services/post_build_generator.dart`
    - `generator_core/pubspec.yaml`
    - `generator_core/test/generator_core_test.dart`

--- generator_core/lib/generator_core.dart
+++
@@ -1,8 +0,0 @@
-/// The `generator_core` package.
-/// Import this file to use the package in your project.
-library;
-
-export 'src/models/builder_metadata.dart';
-export 'src/models/class_metadata.dart';
-export 'src/services/config_reader.dart';
-export 'src/services/post_build_generator.dart';
--- generator_core/lib/src/models/class_metadata.dart
+++
@@ -1,6 +0,0 @@
-abstract class ClassMetadata {
-  final Uri uri;
-  final String name;
-
-  ClassMetadata(this.uri, this.name);
-}
