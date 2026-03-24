130ed264370fc86365b9f1acebb3bcb219e6c3fe
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-03-24 10:48:59 +0600

    DFP-24 Refactor: Remove the `parser_generator` package
    - Deleted the entire `parser_generator` package, including its source code, configuration files, and documentation.
    - Removed `JsonModelCollector` and `LegacyJsonModelCollector` which were responsible for gathering `@JsonCodable` metadata.
    - Removed the `JsonParserGenerator` service and related configuration models (`JsonParserConfig`, `ParserConfig`).
    - Removed the `json_parser_builder` definition and its associated `build.yaml` configuration.
    - Deleted project metadata files including `pubspec.yaml`, `analysis_options.yaml`, `CHANGELOG.md`, and `README.md`.
    
    Note:
    A replacement package was recently created in `json_parser/json_parser_generator` package.
    
    Files deleted:
    - `parser_generator/.fvmrc`
    - `parser_generator/.gitignore`
    - `parser_generator/CHANGELOG.md`
    - `parser_generator/README.md`
    - `parser_generator/analysis_options.yaml`
    - `parser_generator/build.yaml`
    - `parser_generator/example/example.dart`
    - `parser_generator/lib/parser_generator.dart`
    - `parser_generator/lib/src/builders/json_parser/builders/json_parser_builder.dart`
    - `parser_generator/lib/src/builders/json_parser/collectors/json_model_collector.dart`
    - `parser_generator/lib/src/builders/json_parser/collectors/legacy_json_model_collector.dart`
    - `parser_generator/lib/src/builders/json_parser/constants/json_config_constants.dart`
    - `parser_generator/lib/src/builders/json_parser/models/configs/json_parser_config.dart`
    - `parser_generator/lib/src/builders/json_parser/models/configs/parser_config.dart`
    - `parser_generator/lib/src/builders/json_parser/models/json_coder_metadata.dart`
    - `parser_generator/lib/src/builders/json_parser/models/json_parser_builder_metadata.dart`
    - `parser_generator/lib/src/builders/json_parser/services/json_parser_generator.dart`
    - `parser_generator/lib/src/builders/json_parser/services/parser_config_reader.dart`
    - `parser_generator/parser_generator.iml`
    - `parser_generator/pubspec.yaml`
    - `parser_generator/test/parser_generator_test.dart`

--- parser_generator/lib/parser_generator.dart
+++
@@ -1,3 +0,0 @@
-/// The `parser_builder` package.
-/// Import this file to use the package in your project.
-library;
--- parser_generator/lib/src/builders/json_parser/models/json_coder_metadata.dart
+++
@@ -1,7 +0,0 @@
-import 'package:generator_core/generator_core.dart';
-
-class JsonCoderMetadata extends ClassMetadata {
-  final Set<String> parserKeys;
-
-  JsonCoderMetadata(super.uri, super.name, this.parserKeys);
-}
