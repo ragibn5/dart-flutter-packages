49465dbe922ed95031022ae68db52ca2a60cac73
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-03-25 13:12:29 +0600

    DFP-24 WIP-Refactor: Move `CleanArchLintPlugin` to a standalone file and add unit tests
    - Relocated `CleanArchLintPlugin` class from `lib/main.dart` to a new file `lib/src/plugins/clean_arch_lint_plugin.dart` to improve code organization.
    - Updated `lib/main.dart` to import and instantiate the plugin from its new location.
    - Added a new test file `test/lib/src/plugins/clean_arch_lint_plugin_test.dart` to verify that `DependencyDirectionRule` is correctly registered as a warning rule.
    
    Files added:
    - `clean_arch_lint/lib/src/plugins/clean_arch_lint_plugin.dart`
    - `clean_arch_lint/test/lib/src/plugins/clean_arch_lint_plugin_test.dart`
    
    Files modified:
    - `clean_arch_lint/lib/main.dart`

--- clean_arch_lint/lib/main.dart
+++ clean_arch_lint/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:analysis_server_core/analysis_server_core.dart';
-import 'package:clean_arch_lint/src/rules/dependency_direction_rule/dependency_direction_rule.dart';
+import 'package:clean_arch_lint/src/plugins/clean_arch_lint_plugin.dart';
 import 'package:clean_arch_lint/src/services/config/clean_arch_lint_config_loader.dart';
 
 /// The plugin entry point.
@@ -10,18 +10,3 @@ import 'package:clean_arch_lint/src/services/config/clean_arch_lint_config_loade
 final plugin = CleanArchLintPlugin(
   SessionDataManager.createNewInstance(CleanArchLintConfigLoader()),
 );
-
-/// The main plugin class.
-class CleanArchLintPlugin extends Plugin {
-  final SessionDataManager _sessionDataManager;
-
-  CleanArchLintPlugin(this._sessionDataManager);
-
-  @override
-  String get name => '$CleanArchLintPlugin';
-
-  @override
-  void register(PluginRegistry registry) {
-    registry.registerWarningRule(DependencyDirectionRule(_sessionDataManager));
-  }
-}
