c3c794095a66b5e60e0a35086f42f051791588f1
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-03-30 15:18:37 +0600

    DFP-24 WIP-Refactor: Modularize parameter validation in `FromJsonStaticMethodVisitor`
    - Introduced `FromJsonStaticMethodVisitorConfig` to allow customizable error messages for different validation failure contexts (e.g., getters, wrong parameter count, invalid types).
    - Refactored `fromJson` parameter validation into a dedicated `_checkParamType` method for better clarity and separation of concerns.
    - Enhanced parameter validation logic to explicitly check for positional parameters and exclude `FieldFormalParameter` and `SuperFormalParameter`.
    - Updated `_parameterType` helper to support `SuperFormalParameter`.
    - Standardized error reporting across parameter checks using the new configuration class.
    
    Files modified:
    - `json_parser/json_parser_analyzer/lib/src/rules/json_parser_requirement_rule/from_json_static_method_visitor.dart`

--- json_parser/json_parser_analyzer/lib/src/rules/json_parser_requirement_rule/from_json_static_method_visitor.dart
+++ json_parser/json_parser_analyzer/lib/src/rules/json_parser_requirement_rule/from_json_static_method_visitor.dart
@@ -5,9 +5,33 @@ import 'package:analyzer/dart/element/type.dart';
 import 'package:json_parser_analyzer/src/models/json_parser_analyzer_config.dart';
 import 'package:meta/meta.dart';
 
+class FromJsonStaticMethodVisitorConfig {
+  final String getterNotAllowedContextMessage;
+  final String missingParamDeclarationContextMessage;
+  final String wrongParamCountContextMessage;
+  final String wrongParamDeclarationTypeContextMessage;
+  final String invalidParamTypeContextMessage;
+
+  FromJsonStaticMethodVisitorConfig({
+    this.getterNotAllowedContextMessage =
+        'static fromJson must not be a getter.',
+    this.missingParamDeclarationContextMessage =
+        'fromJson method must have only one positional parameter of type Map<String, dynamic> or Map<String, Object?>.',
+    this.wrongParamCountContextMessage =
+        'fromJson method must have only one positional parameter of type Map<String, dynamic> or Map<String, Object?>.',
+    this.wrongParamDeclarationTypeContextMessage =
+        'fromJson method must have only one positional parameter of type Map<String, dynamic> or Map<String, Object?>.',
+    this.invalidParamTypeContextMessage =
+        'fromJson method must have only one positional parameter of type Map<String, dynamic> or Map<String, Object?>.',
+  });
+}
+
 class FromJsonStaticMethodVisitor {
   @visibleForTesting
   final AnalysisRule rule;
+
+  @visibleForTesting
+  final FromJsonStaticMethodVisitorConfig visitorConfig;
 
   @visibleForTesting
   final RuleSessionContext<JsonParserAnalyzerConfig> sessionContext;
@@ -17,17 +41,24 @@ class FromJsonStaticMethodVisitor {
   FromJsonStaticMethodVisitor(
     AnalysisRule rule,
     RuleSessionContext<JsonParserAnalyzerConfig> sessionContext,
-  ) : this._(rule, sessionContext, CollectionTypeResolverFactory.create());
+  ) : this._(
+        rule,
+        FromJsonStaticMethodVisitorConfig(),
+        sessionContext,
+        CollectionTypeResolverFactory.create(),
+      );
 
   @visibleForTesting
   FromJsonStaticMethodVisitor.test(
     AnalysisRule rule,
+    FromJsonStaticMethodVisitorConfig visitorConfig,
     RuleSessionContext<JsonParserAnalyzerConfig> sessionContext,
     CollectionTypeResolver collectionTypeResolver,
-  ) : this._(rule, sessionContext, collectionTypeResolver);
+  ) : this._(rule, visitorConfig, sessionContext, collectionTypeResolver);
 
   FromJsonStaticMethodVisitor._(
     this.rule,
+    this.visitorConfig,
     this.sessionContext,
     this._collectionTypeResolver,
   );
@@ -36,45 +67,19 @@ class FromJsonStaticMethodVisitor {
     MethodDeclaration fromJsonMethod,
     ClassDeclaration enclosingClass,
   ) {
-    final params = fromJsonMethod.parameters?.parameters;
-    if (fromJsonMethod.isGetter || params == null) {
+    if (fromJsonMethod.isGetter) {
       rule.reportAtToken(
         fromJsonMethod.name,
-        arguments: ['static fromJson must not be a getter.'],
+        arguments: [visitorConfig.getterNotAllowedContextMessage],
       );
 
-      // If it is a getter our param specific checks are not needed.
+      // If it is a getter, it won't have any params,
+      // hence our param specific checks are not needed.
       // So, will return.
       return;
     }
 
-    if (params.length != 1) {
-      rule.reportAtNode(
-        fromJsonMethod.parameters,
-        arguments: [
-          'fromJson method must have only one parameter of type Map<String, dynamic> or Map<String, Object?>.',
-        ],
-      );
-    }
-
-    final paramType = _parameterType(params.first);
-    if (paramType == null) {
-      rule.reportAtNode(
-        params.owner,
-        arguments: [
-          'fromJson method must have only one parameter of type Map<String, dynamic> or Map<String, Object?>.',
-        ],
-      );
-    }
-
-    if (!_isJsonMap(paramType)) {
-      rule.reportAtNode(
-        paramType,
-        arguments: [
-          'fromJson method must have only one parameter of type Map<String, dynamic> or Map<String, Object?>.',
-        ],
-      );
-    }
+    _checkParamType(fromJsonMethod);
 
     final returnType = fromJsonMethod.returnType;
     if (returnType == null) {
@@ -83,8 +88,8 @@ class FromJsonStaticMethodVisitor {
         arguments: ['fromJson method must have an explicit return type.'],
       );
 
-      // If the return type was not declared, checks related
-      // to return type's type is irrelevant. So, will return.
+      // If the return type was not declared, further checks
+      // related to return type's type is irrelevant. So, will return.
       return;
     }
 
@@ -92,6 +97,55 @@ class FromJsonStaticMethodVisitor {
       rule.reportAtNode(
         returnType,
         arguments: ['fromJson method must return the enclosing class type.'],
+      );
+    }
+  }
+
+  void _checkParamType(MethodDeclaration fromJsonMethod) {
+    final params = fromJsonMethod.parameters?.parameters;
+    if (params == null) {
+      rule.reportAtToken(
+        fromJsonMethod.name,
+        arguments: [visitorConfig.missingParamDeclarationContextMessage],
+      );
+
+      // This can only happen if the method is a getter, on which case
+      // the FormalParameterList will be null. Although we checked for
+      // getter previously, this one is merely for certainty. Also, if
+      // there are not params, there is no further param specific checks
+      // needed. So, will return.
+      return;
+    }
+
+    if (params.length != 1) {
+      rule.reportAtNode(
+        fromJsonMethod.parameters,
+        arguments: [visitorConfig.wrongParamCountContextMessage],
+      );
+
+      // This check should hide other param specific checks,
+      // because we need to have right number of parameters
+      // to begin with. So, will return.
+      return;
+    }
+
+    if (!_isValidParam(params.first)) {
+      rule.reportAtNode(
+        params.owner,
+        arguments: [visitorConfig.wrongParamDeclarationTypeContextMessage],
+      );
+
+      // This check should hide other param specific checks,
+      // because we need to have right parameter declaration
+      // type to begin with. So, will return.
+      return;
+    }
+
+    final paramType = _parameterType(params.first);
+    if (!_isJsonMap(paramType)) {
+      rule.reportAtNode(
+        paramType,
+        arguments: [visitorConfig.invalidParamTypeContextMessage],
       );
     }
   }
@@ -116,11 +170,18 @@ class FromJsonStaticMethodVisitor {
     return isObjectValueTypedJsonMap || isDynamicValueTypedJsonMap;
   }
 
+  bool _isValidParam(FormalParameter param) {
+    return param.isPositional &&
+        param is! FieldFormalParameter &&
+        param is! SuperFormalParameter;
+  }
+
   TypeAnnotation? _parameterType(FormalParameter param) {
     final actual = param is DefaultFormalParameter ? param.parameter : param;
     return switch (actual) {
       final SimpleFormalParameter p => p.type,
       final FieldFormalParameter p => p.type,
+      final SuperFormalParameter p => p.type,
       _ => null,
     };
   }
