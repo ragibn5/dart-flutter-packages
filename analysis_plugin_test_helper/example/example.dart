e3a981d3e2d8b1885c25d704671e9bffb2869196
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-04-03 20:13:55 +0600

    DFP-4 Update: Update `example.dart` in `analysis_plugin_test_helper`
    - Replaced the empty `main` function with a comment directing users to the `test` folder for usage examples.
    
    Files modified:
    - `analysis_plugin_test_helper/example/example.dart`

--- analysis_plugin_test_helper/example/example.dart
+++ analysis_plugin_test_helper/example/example.dart
@@ -1,1 +1,4 @@
-void main() {}
+// See tests in `test` folder for complete examples.
+//
+// This package is specifically designed to help write tests
+// and so the best example is to provide see the tests themselves.
