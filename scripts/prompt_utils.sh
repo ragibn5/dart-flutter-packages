68fafe4ede79c870ac56bdb64389018c57cee9c3
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-07-08 01:24:12 +0600

    DFP-42 WIP-Refactor: Improve user input handling in `confirm_yes_no` utility
    - Modify `confirm_yes_no` in `scripts/prompt_utils.sh` to allow multi-character input instead of automatically proceeding after a single keypress.
    - Update the input validation logic to support full "yes" and "YES" responses.
    - Clean up redundant `echo` commands and trailing whitespace within the script.

--- scripts/prompt_utils.sh
+++ scripts/prompt_utils.sh
@@ -1,13 +1,9 @@
 confirm_yes_no() {
   local question="$1"
 
-  read -rp "$question [y/n]: " -n 1 response
-  echo
+  read -rp "$question [y/n]: " response
   case "$response" in
     [yY]|[yY][eE][sS]) return 0 ;;
     *) return 1 ;;
   esac
 }
-
-
-
