c287605ec9d7a746e2ac875d43b9e0794b606d30
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-07-26 19:10:54 +0600

    Removed test codes.

--- app_template/lib/features/test/domain/test_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:json_parser_annotations/json_parser_annotations.dart';
-
-@GenerateJsonParser()
-class TestModel {
-  final String name;
-  final int value;
-
-  TestModel({required this.name, required this.value});
-}
